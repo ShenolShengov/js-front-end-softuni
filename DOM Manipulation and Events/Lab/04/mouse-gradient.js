3822f2f978fe8959c6fc82491d9d64cb05598520
Shenol07 <shenolshengov41@gmail.com>
2024-11-22 22:14:43 +0200

    Add soliton for tasks 04 - 05 for DOM Manipulation and Events - Lab

--- DOM Manipulation and Events/Lab/04/mouse-gradient.js
+++ DOM Manipulation and Events/Lab/04/mouse-gradient.js
@@ -1,3 +1,11 @@
 function attachGradientEvents() {
-    //TODO
+    const gradient = document.querySelector('#gradient');
+
+    function showPercentage(e) {
+        const percentage  = Math.floor((e.offsetX / e.target.clientWidth) * 100);
+        document.querySelector('#result').textContent = `${percentage}%`;
+    }
+
+    gradient.addEventListener('mousemove', showPercentage);
+
 }
--- DOM Manipulation and Events/Lab/05/hightlight-active.js
+++ DOM Manipulation and Events/Lab/05/hightlight-active.js
@@ -1,5 +1,18 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+    const inputs = document.querySelectorAll('input[type="text"]');
+
+    [...inputs].forEach(e => {
+        e.addEventListener('focus', addFocus);
+        e.addEventListener('blur', removeFocus);
+    });
+
+    function addFocus(e) {
+        e.target.parentElement.classList.add('focused');
+    }
+
+    function removeFocus(e) {
+        e.target.parentElement.classList.remove('focused');
+    }
 }
