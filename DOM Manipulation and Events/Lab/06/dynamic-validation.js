9c6cc97f2450d860e9d6dc22b1e770d5cd4f391b
Shenol07 <shenolshengov41@gmail.com>
2024-11-24 09:42:32 +0200

    Add soliton for tasks 06 - 07 for DOM Manipulation and Events - Lab

--- DOM Manipulation and Events/Lab/06/dynamic-validation.js
+++ DOM Manipulation and Events/Lab/06/dynamic-validation.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', solve);
 
 function solve() {
-    //TODO
+
+    function checkValidity(e) {
+        const emailRegex = /\w+@\w+\.\w+/gm;
+        const currentEl = e.target;
+        if(!emailRegex.test(currentEl.value)){
+            currentEl.classList.add('error');
+        } else {
+            currentEl.classList.remove('error');
+        }
+    }
+
+    const emailRegex = /\w+@\w+\.\w+/gm;
+
+    const emailEl = document.querySelector('#email');
+
+    emailEl.addEventListener('change', checkValidity);
 }
--- DOM Manipulation and Events/Lab/07/shopping-cart.js
+++ DOM Manipulation and Events/Lab/07/shopping-cart.js
@@ -1,5 +1,47 @@
 document.addEventListener('DOMContentLoaded', solve);
+console.log('Mine');
+
 
 function solve() {
-   //TODO
+
+   function addProduct(e) {
+      const resultEl = document.querySelector('textarea');
+      const productEl = e.target.parentElement.parentElement;
+      const productName = productEl.querySelector('.product-title').textContent;
+      const productPrice = Number(productEl.querySelector('.product-line-price').textContent).toFixed(2);
+
+      resultEl.value = resultEl.value + `Added ${productName} for ${productPrice} to the cart.\n`;
+   }
+
+   function disableButtons(e) {
+      [...document.querySelectorAll('button')].forEach(e => e.disabled = true);
+   }
+
+   function showTotalPrice(e) {
+      const regex = /(?<=Added )\w+|(?<= for )\d.\d+(?= to the cart)/gm;
+      const products = [];
+      const textarea = document.querySelector('textarea');   
+      
+
+      textarea.value.trim().split('\n').filter(e => e).forEach(e => {
+         const [name, price] = e.match(regex);
+         products[name] = (products[name] || 0) + Number(price);
+      })
+      
+      const productsNames = Object.keys(products).join(', ');
+      const totalPrice = Object.values(products).reduce((total, c) => total + c, 0);
+      textarea.value = textarea.value + `You bought ${productsNames} for ${totalPrice.toFixed(2)}.`;
+      textarea.value = textarea.value.trim();
+   }
+
+   
+
+   const addButtons = document.querySelectorAll('.add-product');
+   [...addButtons].forEach(e => e.addEventListener('click', addProduct));
+
+   const checkoutBtn = document.querySelector('.checkout');
+   checkoutBtn.addEventListener('click', showTotalPrice);
+   checkoutBtn.addEventListener('click', disableButtons);
+
+
 }
