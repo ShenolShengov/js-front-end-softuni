e49c3e37bdbaa5bcfad9a469236b85893d2a4720
Shenol07 <shenolshengov41@gmail.com>
2024-11-26 18:21:42 +0200

    Add resources for DOM Manipulaiton and Events - Exercises

---
+++ DOM Manipulation and Events/Exercises/01/sections.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', solve);
+
+function solve() {
+   const formEl = document.querySelector('#task-input');
+
+   formEl.addEventListener('submit', generateBoxes);
+
+
+   function generateBoxes(e){
+      e.preventDefault();
+      const elementsText = e.currentTarget.querySelector('input[type="text"').value.split(',').filter(e => e.trim());
+
+      const contentEl = document.querySelector('#content');
+      elementsText.forEach(text => addBox(text, contentEl));
+   }
+
+   function addBox(text, contentEl){
+      contentEl.appendChild(createBox(text));
+   }
+
+   function createBox(text){
+      const boxEl = document.createElement('div');
+      const pEl = document.createElement('p');
+      pEl.textContent = text.trim();
+      boxEl.appendChild(pEl);
+      return boxEl;
+   }
+}
